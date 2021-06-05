32d0c55afb4d4170ff093ce101cd5106086f63e1
Cyl3el2Cleal2 <natha_sk@hotmail.com>
2021-06-06 02:12:28 +0700

    add get, post, put games route

--- microservice/game/src/route.py
+++ microservice/game/src/route.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, status, HTTPException
+from models import GameCreateBody, GameUpdateBody, PickCardResponse
+from service import GameService
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
-@router.get('/game')
-def game():
-  return 'Welcome to game api'
+gameservice = GameService()
+
+@router.get('/games/{game_id}')
+async def get_game(game_id: str):
+  game = await gameservice.get_game(game_id)
+  if game is None:
+    raise HTTPException(status.HTTP_404_NOT_FOUND, f"Not found game id : {game_id}")
+  return game
+
+@router.post('/games')
+async def create_game(game: GameCreateBody = Body(...)):
+  new_game = gameservice.create_game(game.player)
+  result = await gameservice.save_game(jsonable_encoder(new_game))
+  return result
+
+@router.put('/games')
+async def open_card(data: GameUpdateBody = Body(...)):
+  updating = await gameservice.pick_card(data)
+  return updating
