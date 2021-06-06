ba0cec754bae9c1b68403868f32f47b340d794e8
Cyl3el2Cleal2 <natha_sk@hotmail.com>
2021-06-06 15:30:24 +0700

    add responce_model for route '/game'

--- microservice/game/src/route.py
+++ microservice/game/src/route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Body, status, HTTPException
-from models import GameCreateBody, GameUpdateBody, PickCardResponse
+from models import GameShow, GameCreateBody, GameUpdateBody, PickCardResponse
 from service import GameService
 from fastapi.encoders import jsonable_encoder
 
@@ -7,20 +7,24 @@ router = APIRouter()
 
 gameservice = GameService()
 
-@router.get('/games/{game_id}')
+
+@router.get('/games/{game_id}', response_model=GameShow)
 async def get_game(game_id: str):
-  game = await gameservice.get_game(game_id)
-  if game is None:
-    raise HTTPException(status.HTTP_404_NOT_FOUND, f"Not found game id : {game_id}")
-  return game
+    game = await gameservice.get_game(game_id)
+    if game is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            f"Not found game id : {game_id}")
+    return game
 
-@router.post('/games')
+
+@router.post('/games', response_model=GameShow)
 async def create_game(game: GameCreateBody = Body(...)):
-  new_game = gameservice.create_game(game.player)
-  result = await gameservice.save_game(jsonable_encoder(new_game))
-  return result
+    new_game = gameservice.create_game(game.player)
+    result = await gameservice.save_game(jsonable_encoder(new_game))
+    return result
 
-@router.put('/games', response_model= PickCardResponse)
-async def open_card(data: GameUpdateBody = Body(...)):
-  updating = await gameservice.pick_card(data)
-  return updating
+
+@router.put('/games', response_model=PickCardResponse)
+async def pick_card(data: GameUpdateBody = Body(...)):
+    updating = await gameservice.pick_card(data)
+    return updating
